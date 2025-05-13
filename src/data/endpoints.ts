8fa24109c9521a473886147cdd39fdb19fa66bd2
rbmpsh <rbmpsh@gmail.com>
2025-05-13 08:49:19 +0800

    fix: api host

--- src/data/endpoints.ts
+++ src/data/endpoints.ts
@@ -1,8 +1,10 @@
+import { ConfigValue } from "@/config";
+
 export const API_ENDPOINTS = {
-  USERS_ME: "http://localhost:4001/b2b/auth/me",
-  USERS_LOGIN: "http://localhost:4001/b2b/login",
+  USERS_ME: `${ConfigValue.NEXT_PUBLIC_REST_API_ENDPOINT}/b2b/auth/me`,
+  USERS_LOGIN: `${ConfigValue.NEXT_PUBLIC_REST_API_ENDPOINT}/b2b/login`,
   USERS_LOGOUT: "/logout",
-  USERS_DATA: "http://localhost:4001/b2b/auth/data",
+  USERS_DATA: `${ConfigValue.NEXT_PUBLIC_REST_API_ENDPOINT}/b2b/auth/data`,
   BUYERS_DATA: "http://localhost:3001/buyers",
   SELLERS_DATA: "http://localhost:3001/sellers",
 };
