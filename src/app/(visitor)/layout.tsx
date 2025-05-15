903affd31fccb6fad5534a26e6f2a5555abd2d98
rbmpsh <rbmpsh@gmail.com>
2025-05-15 09:07:59 +0800

    update: remove footer

--- src/app/(visitor)/layout.tsx
+++ src/app/(visitor)/layout.tsx
@@ -1,4 +1,3 @@
-import { Footer } from "@/components/footer/footer";
 import { Header } from "@/components/header/header";
 
 export default function UserAdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
@@ -6,7 +5,6 @@ export default function UserAdminLayout({ children }: Readonly<{ children: React
     <div className="pt-32">
       <Header />
       {children}
-      <Footer />
     </div>
   );
 }
