d6af65d861087029c20c140ffca255e9abe34f2d
rbmpsh <rbmpsh@gmail.com>
2025-05-19 10:21:45 +0800

    fix: type error

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import { Button } from "@headlessui/react";
 
-type CustomButtonProps = { children?: React.ReactNode; component?: React.ElementType; className?: string; loading?: boolean; onClick?: () => void; href?: string };
+type CustomButtonProps = { children?: React.ReactNode; component?: React.ElementType; className?: string; loading?: boolean; onClick?: () => void; href?: string | URL };
 export const CustomButton: React.FC<CustomButtonProps> = ({ component: Component = Button, className, loading, children, ...rest }) => {
   return (
     <Component className={clsx(className, "cursor-pointer", loading && "cursor-not-allowed")} {...rest}>
