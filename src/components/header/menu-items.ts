81074ae0c966cc6e41a4d6c8f4b3827872fdb312
rbmpsh <rbmpsh@gmail.com>
2025-05-23 11:21:35 +0800

    fix: use config Routes

--- src/components/header/menu-items.ts
+++ src/components/header/menu-items.ts
@@ -1,3 +1,4 @@
+import { Routes } from "@/config/routes";
 import { countries } from "@/data/countries";
 import { MenuItemType } from "@/types";
 
@@ -5,17 +6,17 @@ export const menuItemsDesktop: MenuItemType[] = [
   {
     key: "Country / Region",
     label: "Country / Region",
-    items: countries.map(({ label, code }) => ({ key: label, label: label, href: `/result?company_location=${code}` })),
+    items: countries.map(({ label, code }) => ({ key: label, label: label, href: `${Routes.public.result}?company_location=${code}` })),
   },
   {
     key: "Partnership Types",
     label: "Partnership Types",
-    items: [{ key: "技術合作", label: "技術合作", href: "/result?partnership_looking_for=技術合作" }],
+    items: [{ key: "技術合作", label: "技術合作", href: `${Routes.public.result}?partnership_looking_for=技術合作` }],
   },
   {
     key: "Industry",
     label: "Industry",
-    items: [{ key: "先進材料", label: "先進材料", href: "/result?industry_classification=先進材料" }],
+    items: [{ key: "先進材料", label: "先進材料", href: `${Routes.public.result}?industry_classification=先進材料` }],
   },
 ];
 
@@ -24,11 +25,12 @@ export const menuItemsRest: MenuItemType[] = [
     key: "Pages",
     label: "Pages",
     items: [
-      { key: "Profile_green", label: "Profile green", href: "/profile/1" },
-      { key: "Profile_blue", label: "Profile blue", href: "/profile/2" },
-      { key: "Profile_red", label: "Profile red", href: "/profile/3" },
-      { key: "calendar", label: "calendar", href: "/admin" },
-      { key: "result", label: "result", href: "/result" },
+      { key: "Profile_green", label: "Profile green", href: `${Routes.public.profile}/1` },
+      { key: "Profile_blue", label: "Profile blue", href: `${Routes.public.profile}/2` },
+      { key: "Profile_red", label: "Profile red", href: `${Routes.public.profile}/3` },
+      { key: "calendar", label: "calendar", href: Routes.private.admin },
+      { key: "form", label: "form", href: Routes.private.form },
+      { key: "result", label: "result", href: Routes.public.result },
     ],
   },
 ];
--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,3 +1,5 @@
+import { profile } from "console";
+
 export const Routes = {
   auth: {
     signIn: "/sign-in",
@@ -5,9 +7,12 @@ export const Routes = {
   },
   public: {
     home: "/",
+    result: "/result",
+    profile: "/profile",
     notFound: "/404",
   },
   private: {
     admin: "/admin",
+    form: "/admin/buyer-form",
   },
 };
