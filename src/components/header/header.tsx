838ac3a45a76febbbcc35f868c829a0f43c31a7f
rbmpsh <rbmpsh@gmail.com>
2025-05-26 15:31:48 +0800

    fix: desktop header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -65,21 +65,23 @@ const MobileMenu: React.FC<{ items: MenuItemType[] }> = ({ items }) => (
   />
 );
 
-const DesktopMenu: React.FC<{ items: MenuItemType[] }> = ({ items }) => (
-  <div className="hidden sm:flex sm:flex-wrap">
-    {items.map((item, i) => (
-      <Fragment key={item.key}>
-        <NestMenu
-          items={[item]}
-          btn={
-            <>
-              <span className="text-white/80">{item.label}</span>
-              <ChevronDownIcon className="size-4 text-white/80" />
-            </>
-          }
-        />
-        {i !== items.length - 1 && <Splitter className="mx-[1.875rem] md:mx-[3.125rem]" />}
-      </Fragment>
-    ))}
-  </div>
-);
+const DesktopMenu: React.FC<{ items: MenuItemType[] }> = ({ items }) => {
+  return (
+    <div className="hidden sm:flex sm:flex-wrap">
+      {items.map((item, i) => (
+        <Fragment key={item.key}>
+          <NestMenu
+            items={item.items || []}
+            btn={
+              <>
+                <span className="text-white/80">{item.label}</span>
+                <ChevronDownIcon className="size-4 text-white/80" />
+              </>
+            }
+          />
+          {i !== items.length - 1 && <Splitter className="mx-[1.875rem] md:mx-[3.125rem]" />}
+        </Fragment>
+      ))}
+    </div>
+  );
+};
