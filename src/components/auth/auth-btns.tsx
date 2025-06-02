f00299ee62462ce1613b4db6acf04add47f62b05
rbmpsh <rbmpsh@gmail.com>
2025-06-02 11:34:25 +0800

    fix: build error

--- src/components/auth/auth-btns.tsx
+++ src/components/auth/auth-btns.tsx
@@ -6,7 +6,7 @@ import { CustomButton } from "@ui/button";
 import { useAuthBtn } from "@/hooks/use-auth-btn";
 
 export const AuthBtn: React.FC = () => {
-  const { label, key, ...props } = useAuthBtn();
+  const { label, ...props } = useAuthBtn();
   return (
     <CustomButton className="px-6 py-3 bg-b2b-lv4 text-b2b-lv1 rounded-sm text-sm" component={props?.href ? Link : Button} {...props}>
       {label}
